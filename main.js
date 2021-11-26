7c6ab7be98a871c3544f5c3e8b676204122587b5
manan-liao <77179093+manan-liao@users.noreply.github.com>
2021-11-26 18:28:04 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
+video = "";
+status = "";
+
 function setup(){
     canvas = createCanvas(400,350);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+}
 
+function draw(){
+    image(video,0,0,400,350);
+}
+
+function start(){
+    objectDetector = ml5.objectDetector('Cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Object Detected";
+    object_name = document.getElementById("input").value;
+}
+
+function modelLoaded(){
+    console.log("Cocossd is successfully loaded");
+    status = true;
 }
