617a3957428029c549dff369c4361a54d05b1afe
manan-liao <77179093+manan-liao@users.noreply.github.com>
2021-12-02 18:18:23 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 video = "";
 status = "";
+objects = "";
 
 function setup(){
     canvas = createCanvas(400,350);
@@ -9,7 +10,32 @@ function setup(){
 }
 
 function draw(){
-    image(video,0,0,400,350);
+    image(video,0,0,600,400);
+    if(status != ""){
+        objectDetector.detect(video,gotResult);
+        for(i = 0;i<objects.length;i++){
+            document.getElementById("status").innerHTML = "Status: Objects Detected";
+            document.getElementById("number_objects").innerHTML = "Number of objects dected are "+objects.length;
+            fill("red");
+            percent = floor(objects[i].confidence * 100);
+            textSize(20);
+            text(objects[i].label+" "+percent+"%",objects[i].x,objects[i].y);
+            noFill();
+            stroke("blue");
+            rect(objects[i].x,objects[i].y,objects[i].width,
+                objects[i].height);
+        }
+    }
+}
+
+function gotResult(error,results){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(results);
+        objects = results;
+    }
 }
 
 function start(){
